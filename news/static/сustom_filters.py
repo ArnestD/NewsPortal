5b4e49e94bf541c7da1f9489c5dd30642d0da02f
ArnestD <al.muzykant@yandex.ru>
2023-04-21 18:19:42 +0300

    1,1

--- news/static/сustom_filters.py
+++ news/static/сustom_filters.py
@@ -12,20 +12,18 @@ def currency(value):
    """
    value: значение, к которому нужно применить фильтр
    """
-   #"""
-   #value = ["fuck", "bitch", "Заголовок"]
-   #"""
+   chars = ["*","#","%","&","?","@"]
+   bw  = ["fuck", "bitch"]
+   txt = value.lower().split(' ')
+
+   for word in txt:
+      if word in bw:
+         temp = random.sample(chars, len(word))
+         i = ''.join(temp)
+         txt = [x.replace(word, i) for x in txt]
+
+   res = ' '.join(txt)
+
    # Возвращаемое функцией значение подставится в шаблон.
-   return f'*'
+   return f'***'
 
-#chars = ["*","#","%","&","?","@"]
-#bw  = ["fuck", "bitch"]
-#txt = 'Fuck you  fucker bitch'.lower().split(' ')
-
-#for word in txt:
-    #if word in bw:
-        #temp = random.sample(chars, len(word))
-        #i = ''.join(temp)
-        #txt = [x.replace(word, i) for x in txt]
-
-#res = ' '.join(txt)
